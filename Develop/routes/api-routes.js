d0a6fe78c932797fa384114b9f834430ddd80df0
Lizzz <alejandroraidi@gmail.com>
2023-06-01 17:10:23 -0600

    router post now reads file

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -1,10 +1,12 @@
 const apiRouter = require('express').Router();
 const { response } = require('express');
 const fs = require('fs');
+const { parse } = require('path');
 const { v4: uuidv4 } = require('uuid');
 dataBase = require('../db/db.json');
 
 //API ROUTES
+
 //READS THE FILE AND RESPONDS WITH JSON DATA
 apiRouter.get('/notes', (req, res) => {
     fs.readFile('./db/db.json', 'utf-8', (err, data) => {
@@ -15,18 +17,26 @@ apiRouter.get('/notes', (req, res) => {
     });
 });
 
-
+//CHECKS FOR A BODY AND TITLE AND IF IT EXISTS READS THE FILE AND ADDS IT TO THE DB AND RESPONDS WITH THE NEWLY ADDED DATA 
 apiRouter.post('/notes', (req, res) => {
-    let newNote;
     if (req.body.title && req.body.text){
-        newNote = {
+        const newNote = {
             title:req.body.title,
             text:req.body.text,
             id: uuidv4()
         };
-        dataBase.push(newNote);
-        fs.writeFileSync('./db/db.json', JSON.stringify(dataBase, null, 4));
-        res.json(newNote);
+        fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+            let parsedJson = JSON.parse(data);
+            parsedJson.push(newNote);
+
+            fs.writeFile('./db/db.json', JSON.stringify(parsedJson, null, 4), (error) => {
+                if(error){
+                    res.send('error writing to file');
+                }else{
+                    res.json(newNote);
+                }
+            })
+        })
     }else{
         res.status(400).json('Note should contain a title and text');
     }
